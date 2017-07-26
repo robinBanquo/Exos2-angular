13ee986eb08c75236a87e6c09a6b083d00a7d581
robinBanquo <caramaschi@hotmail.com>
2017-07-26 11:35:08 +0200

    fist commit

--- angular/src/app/exos/exos.component.ts
+++ angular/src/app/exos/exos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-exos',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExosComponent implements OnInit {
 
+  //exo1
+  public inputs = [
+    'text',
+    'number',
+    'submit',
+    'color'
+  ];
+
+  public currentType = 0;
+
+  public type = this.inputs[this.currentType];
+
+  //exo2
+  public counter = 0 ;
+
+  //exo3
+  public color = "red";
+
+  //exo4
+  name: string = '';
+  inputText= "";
+
+  /**********************************
+   * methodes
+   **********************************/
+
   constructor() { }
 
+  //exo1
+  private inputRefresh(){
+    this.type = this.inputs[this.currentType];
+  }
+
+  public changeInput() {
+    if (this.currentType < this.inputs.length){
+      this.currentType++;
+    }else{
+      this.currentType = 0
+    }
+    this.inputRefresh()
+  }
+
+  //exo2
+  public count(add){
+    add? this.counter++ : this.counter--;
+  }
+
+  //exo3
+  public changeColor(color){
+    this.color = color
+  }
   ngOnInit() {
   }
+
 
 }
